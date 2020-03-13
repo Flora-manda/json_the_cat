6a59402a9cbc736525d18060d06549782f1733c1
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-13 12:34:48 +0000

    Cat breed API and test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,30 @@
 const request = require('request');
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
-const arg = process.argv.slice(2);
-const breedArg = arg[0];
 
-const catRequest = (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
 
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    if (data[0]["name"] === breedArg) {
-      console.log(data[0]["description"]);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  request(`${url}${breedName}`, (error, response, body) => {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    if (error) {
+      callback(error, null);
+    } else if (JSON.parse(body).status < 200 || JSON.parse(body).status > 299) {
+      callback("Something went wrong", null);
     }
-    if (data[0]["name"] !== breedArg) {
-      console.log("Breed not found!");
+    else {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        callback(null, `${breedName} does not exist!`);
+      }
+      else {
+        return callback(null, data[0]["description"]);
+       } 
     }
-  }
+  });
+
 };
 
-request(url, catRequest);
+
+
+module.exports = { fetchBreedDescription };
